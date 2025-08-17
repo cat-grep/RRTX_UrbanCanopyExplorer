99f4f530f90a6b476f9385da9db79e398d469e33
cat-grep <huang674@wisc.edu>
2025-08-17 14:55:48 -0500

    fix vite config base issue

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/RRTX_UrbanCanopyExplorer/",   // <-- move here (top-level)
   plugins: [react()],
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-    base: "/RRTX_UrbanCanopyExplorer/",
-  },
+  server: { open: true },
+  build: { outDir: "dist" },
 });
